dbcb0f7b5474af4714b4ede1d8b06605859d5fff
sandrewtx08 <sandrewtx08@protonmail.com>
2022-07-24 19:52:36 -0300

    refactor: transaction pipes

--- src/transactions/transactions.pipe.ts
+++ src/transactions/transactions.pipe.ts
@@ -1,36 +1,30 @@
 import { Type } from 'class-transformer';
 import { IsInt, Max } from 'class-validator';
 
-export class TransactionTransferPipe {
+export class TransactionPipe {
+  @IsInt()
+  @Type(() => Number)
+  from_account_id: number;
+}
+
+export class TransactionTransferPipe extends TransactionPipe {
   @IsInt()
   @Type(() => Number)
   to_account_id: number;
 
   @IsInt()
   @Type(() => Number)
-  from_account_id: number;
-
-  @IsInt()
-  @Type(() => Number)
   amount: number;
 }
 
-export class TransactionDepositPipe {
-  @IsInt()
-  @Type(() => Number)
-  from_account_id: number;
-
+export class TransactionDepositPipe extends TransactionPipe {
   @IsInt()
   @Max(2000)
   @Type(() => Number)
   amount: number;
 }
 
-export class TransactionBuyPipe {
-  @IsInt()
-  @Type(() => Number)
-  from_account_id: number;
-
+export class TransactionBuyPipe extends TransactionPipe {
   @IsInt()
   @Type(() => Number)
   product_id: number;

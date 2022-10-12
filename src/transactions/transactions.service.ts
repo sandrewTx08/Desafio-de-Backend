3c5dc5f813958b485a05a554ce9bce3555b4c36b
sandrewtx08 <sandrewtx08@protonmail.com>
2022-10-12 15:21:35 -0300

    refactor(transactions): transaction code

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -94,7 +94,7 @@ export class TransactionsService {
     const { fee_percentage, fee_fixed } =
       await this.accountTransactionTypeService.findOne({
         account_type_id: from.account_type_id,
-        transaction_type_id: 1,
+        transaction_type: TransactionCode.DEPOSIT,      
       });
 
     const balance = from.balance.toNumber();
